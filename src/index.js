1c080ec5d8b3cb822b340d66ad2ba92014cf2422
Zedwag <zedwag@gmail.com>
2020-09-30 01:51:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoded = ''
+    expr.split('**********').forEach(word => {
+        let output = ''
+        for (let i = 0; i < word.length; i+= 10) {
+            let substr = word.substr(i, 10).split('').reverse().join('')
+            let code =''
+            for (let j = 0; j < 10; j += 2) {
+                if (substr.substr(j, 2) === '01') code = '.' + code
+                else if (substr.substr(j, 2) === '11') code = '-' + code
+            }
+            if (MORSE_TABLE[code]) output += MORSE_TABLE[code]
+        }
+        decoded += output + ' '
+    })
+    return decoded.trim()
 }
 
 module.exports = {
